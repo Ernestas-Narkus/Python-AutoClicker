3ccd5554c65e2d0e4a001dd0ddbfa1f08d0e92e8
Ernestas-Narkus <ernestas.nar@gmail.com>
2025-08-01 19:24:22 +0100

    Add files via upload

---
+++ cursor.py
@@ -0,0 +1,27 @@
+import pyautogui
+import keyboard
+import time
+
+# python -i "$(FULL_CURRENT_PATH)"
+
+def get_pos():
+    position_x, position_y = pyautogui.position()
+    return position_x, position_y
+    
+def amountToClick(x, y):
+    pyautogui.click(x,y)
+
+def main():
+    run = True
+    print("Please input what key you want to start the process" )
+    pressed_key = keyboard.read_key()
+    print(f"Using key {pressed_key}, press esc to exit")
+    while run == True:
+        if keyboard.is_pressed(pressed_key):
+            x,y = get_pos()
+            amountToClick(x,y)
+        elif keyboard.is_pressed('esc'):
+            run = False
+        
+
+main()
